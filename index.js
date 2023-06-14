ec35216304a0c64b06ce9c65626aa42a3c6f5e26
IlhamR286 <ilham.ramdhan286@gmail.com>
2023-06-14 13:19:56 +0700

    Menambahkan state.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 console.log("Menyalakan mesin kopi");
 console.log("Menggiling biji kopi");
 console.log("Memanaskan air");
@@ -5,3 +6,16 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+*/
+const { coffeStock, isCoffeMachineReady } = require("./state");
+// console.log(coffeStock);
+const makeCoffe = (type, miligrams) => {
+  if (coffeStock[type] >= miligrams && isCoffeMachineReady == true) {
+    console.log("Mesin Kopi Sudah Siap");
+    console.log("Kopi ini berhasil di buat!");
+  } else {
+    console.log("Mohon maaf kopi telah habis");
+  }
+};
+
+makeCoffe("robusta", 70);
